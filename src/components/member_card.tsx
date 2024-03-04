67351c8ce392a3ee05ddae479ec2d8a94f2bbb2f
Rounak Sen <103501472+rony0000013@users.noreply.github.com>
2024-03-04 22:38:30 +0530

    Update team and member data

--- src/components/member_card.tsx
+++ src/components/member_card.tsx
@@ -1,47 +1,106 @@
 import { Avatar, Tooltip } from "@nextui-org/react";
-import {Modal, ModalContent, ModalHeader, ModalBody, useDisclosure} from "@nextui-org/react";
-
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  useDisclosure,
+  Image,
+  AvatarIcon,
+} from "@nextui-org/react";
+import Github from "../assets/github_icon.svg";
+import LinkedIn from "../assets/LinkedIn_icon.svg";
+import user from "../assets/user.png";
 
 interface MemberCardProps {
-  key: number;
+  key?: number;
   username: string;
   img: string;
   type: string;
-  url: string;
-  pos: string;
+  pos?: string;
+  linkedIn?: string;
+  gitHub?: string;
 }
-export default function MemberCard({ key, username, img, type, pos }: MemberCardProps) {
-    const {isOpen, onOpen, onClose} = useDisclosure();
+export default function MemberCard({
+  key,
+  username,
+  img,
+  type,
+  pos,
+  linkedIn,
+  gitHub,
+}: MemberCardProps) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
-    <div  className="m-4 flex flex-col items-center" onClick={() => onOpen()}>
-      <Tooltip showArrow={true} color="foreground" content={username} >
-        <Avatar
-          key={key}
-          isBordered
-          showFallback
-          size={type==="member" ? "lg" : undefined}
-          src={img}
-          name={username}
-          alt={username}
-          className={`text-large ${type==="member" ? "" : "sm:h-[10vw] sm:w-[10vw] h-[30vw] w-[30vw]"}`}
-        />
-      </Tooltip>
-    </div>
+      <div className="m-4 flex flex-col items-center" onClick={() => onOpen()}>
+        <Tooltip showArrow={true} color="foreground" content={username}>
+          <Avatar
+            key={key}
+            isBordered
+            showFallback
+            size={type === "member" ? "lg" : undefined}
+            src={img}
+            name={username}
+            alt={username}
+            className={`text-large ${
+              type === "member"
+                ? ""
+                : "h-[30vw] w-[30vw] sm:h-[10vw] sm:w-[10vw]"
+            }`}
+          />
+        </Tooltip>
+      </div>
 
-<Modal backdrop='blur' placement="center" isOpen={isOpen} onClose={onClose}>
-<ModalContent>
-  {(onClose) => (
-    <>
-      <ModalHeader className="flex flex-col gap-1 items-center">{username}</ModalHeader>
-      <ModalBody>
-        <h2 className="text-center">{pos}</h2>
-      </ModalBody>
+      <Modal
+        backdrop="blur"
+        placement="center"
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col items-center gap-1">
+                {username}
+              </ModalHeader>
+              <ModalBody>
+                <div className="flex flex-col items-center justify-center text-center">
+                  <Image
+                    isBlurred
+                    src={img}
+                    loading="lazy"
+                    fallbackSrc={user.src}
+                    alt={username}
+                    className="text-large mx-auto max-h-[50vh]"
+                  />
+                  <div className="h-2" />
+                  {pos && <h2 className="text-center">{pos}</h2>}
+                  <div className="flex gap-10 p-2">
+                    <a href={gitHub} target="_blank">
+                      <img
+                        src={Github.src}
+                        alt="Github"
+                        width={50}
+                        height={50}
+                      />
+                    </a>
+                    <a href={linkedIn} target="_blank">
+                      <img
+                        src={LinkedIn.src}
+                        alt="LinkedIn"
+                        width={50}
+                        height={50}
+                      />
+                    </a>
+                  </div>
+                </div>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </>
-  )}
-</ModalContent>
-</Modal>
-</>
   );
 }
